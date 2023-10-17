03ac6b01e3b3c7c08b789cf1b4bb59c1065296cd
Jay Eskandarian <jeskandarian@expedia.com>
2023-10-17 09:08:50 -0700

    initial commit

---
+++ src/main/kotlin/com/expediagroup/interview/springboot/InterviewSpringBootApplication.kt
@@ -0,0 +1,11 @@
+package com.expediagroup.interview.springboot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InterviewSpringBootApplication
+
+fun main(args: Array<String>) {
+    runApplication<InterviewSpringBootApplication>(*args)
+}
---
+++ src/test/kotlin/com/expediagroup/interview/springboot/InterviewSpringBootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.expediagroup.interview.springboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InterviewSpringBootApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
